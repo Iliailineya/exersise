2481fb7d52f7cbc9d4aeec314471d9b41439e037
Iliailineya <ilavituk011@gmail.com>
2023-03-31 18:50:22 +0300

    Prime number and MethodExecutionTime (didt working)

--- src/Problem.java
+++ src/Problem.java
@@ -4,14 +4,13 @@ public class Problem {
 
     //Задача на поиск простых чисел:
     // Напишите программу, которая принимает на вход число N и выводит все простые числа от 1 до N.
-    public static void one() throws Exception {
+    public static void one() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите число:");
         try {
             int num = scanner.nextInt();
-            List list = (List)Solutions.measureMethodExecutionTime("primeNumbers");
 
-            System.out.println("\nПростые числа от 1 до " + num + ":\n" + list);
+            System.out.println("\nПростые числа от 1 до " + num + ":\n" + Solutions.primeNumbers(num));
         } catch (
                 InputMismatchException e) {
             System.out.println("Ошибка: вы ввели неверный формат числа");
